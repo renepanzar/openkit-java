372093268dc57003dc42e05542d98718b6749e0a
Johannes Baeuerle <johannes.baeuerle@dynatrace.com>
2018-04-20 13:07:55 +0200

    return byte arrays in serializers

--- src/main/java/com/dynatrace/openkit/protocol/JsonSerializer.java
+++ src/main/java/com/dynatrace/openkit/protocol/JsonSerializer.java
@@ -3,6 +3,6 @@ package com.dynatrace.openkit.protocol;
 public class JsonSerializer implements Serializer {
     @Override
     public byte[] serialize(Payload data) {
-
+        return new byte[1];
     }
 }
--- src/main/java/com/dynatrace/openkit/protocol/MobileProtocolV3Serializer.java
+++ src/main/java/com/dynatrace/openkit/protocol/MobileProtocolV3Serializer.java
@@ -3,6 +3,6 @@ package com.dynatrace.openkit.protocol;
 public class MobileProtocolV3Serializer implements Serializer {
     @Override
     public byte[] serialize(Payload data) {
-
+        return new byte[1];
     }
 }
